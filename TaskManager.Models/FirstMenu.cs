fde86d91a08f0b1b88ff051c1d7d8062487ebf12
Sanjik <Beiseev_193@mail.ru>
2019-06-15 18:37:04 +0600

    v1

---
+++ TaskManager.Models/FirstMenu.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManager.Models
+{
+	public class FirstMenu
+	{
+		//----------- for Windows 10 ----------
+
+	    public int Id { get; set; }                 //Id
+		public string ApplicationName { get; set; } //Приложение
+		public string State { get; set; }           //Состояние
+		public double CPU { get; set; }             //ЦП %
+		public double RAM { get; set; }             //Память
+		public double DiskSpeed { get; set; }       //Диск
+		public double NetSpeed { get; set; }        //Сеть
+		public double GPU { get; set; }             //GPU
+		public string Kernel3D { get; set; }        //ядро графического процессора
+
+		//----------- for Windows 7 --------------
+
+		//public string ApplicationName { get; set; }
+		//public int CPU { get; set; }
+		//public int RAM { get; set; }
+		//public string Description { get; set; }
+		//public string Bystrodeistvie { get; set; }
+		//public string Network { get; set; }
+		//public string Users { get; set; }
+	}
+}
---
+++ TaskManager/MainWindow.xaml.cs
@@ -0,0 +1,98 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using TaskManager.Models;
+
+namespace TaskManager
+{
+	/*
+	 * Создать с помощью WPF диспетчер задач, в котором выводится информация по процессам.
+	 * Придусмотреть кнопку снятие задачи.
+	 * При возникновении ошибки во время остановки процесса, уведомить пользователя, но не завершать работу.
+	 */
+	public partial class MainWindow : Window
+	{
+		public MainWindow()
+		{
+			InitializeComponent();
+			GetAllProcesses();
+		}
+		private void GetAllProcesses()
+		{
+			Process[] processes = Process.GetProcesses();
+			List<FirstMenu> menu = new List<FirstMenu>();
+			processTask.CanUserReorderColumns = false;
+
+			foreach (var process in processes)
+			{
+				PerformanceCounter performanceCPU = new PerformanceCounter();
+				performanceCPU.CategoryName = "Process";
+				performanceCPU.CounterName = "% Processor Time";
+				performanceCPU.InstanceName = process.ProcessName;
+
+				PerformanceCounter performanceDisk = new PerformanceCounter();
+				performanceDisk.CategoryName = "PhysicalDisk";
+				//performanceDisk.CounterName = "% Disk Time";
+				//performanceDisk.CounterName = "Disk Reads/sec";
+				performanceDisk.CounterName = "Disk Writes/sec";
+				performanceDisk.InstanceName = process.ProcessName;
+				FirstMenu oneMenu = new FirstMenu();
+				//oneMenu.ApplicationName = process.MainWindowTitle;
+				oneMenu.Id = process.Id;
+				oneMenu.ApplicationName = process.ProcessName;
+				oneMenu.State = process.Responding.ToString();
+				//Task task = new Task(() =>
+				//{
+				//	oneMenu.CPU = Math.Round(performanceCPU.NextValue(), 2);
+				//});
+				//oneMenu.CPU = Math.Round(performanceCPU.NextValue(), 2);
+				oneMenu.RAM = process.WorkingSet64 / (1024 * 1024);
+				//oneMenu.DiskSpeed = performanceDisk.NextValue();
+				//oneMenu.NetSpeed =;
+				//oneMenu.GPU =;
+				//oneMenu.Kernel3D =;
+				menu.Add(oneMenu);
+			}
+			processTask.ItemsSource = menu;
+		}
+		private void ButtonClick(object sender, RoutedEventArgs e)
+		{
+			if (processTask.SelectedItem == null)
+			{
+				MessageBox.Show($"Choose process");
+			}
+			else
+			{
+				try
+				{
+					FirstMenu menu = (FirstMenu)processTask.SelectedItem;
+					List<Process> processes = Process.GetProcesses().ToList<Process>();
+					foreach (var process in processes)
+					{
+						if (process.ProcessName == menu.ApplicationName && process.Id == menu.Id)
+						{
+							//process.Kill();
+							MessageBox.Show($"Процесс с именем: {menu.ApplicationName} с индексом {menu.Id} удалён");
+						}
+					}
+					GetAllProcesses();
+				}
+				catch (Exception exception)
+				{
+					MessageBox.Show($"Error: {exception}");
+				}
+			}
+		}
+
+		//Menu Exit
+		private void MenuItemClick(object sender, RoutedEventArgs e)
+		{
+			Application.Current.Shutdown();
+		}
+	}
+}
